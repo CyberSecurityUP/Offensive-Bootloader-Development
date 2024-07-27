70fd4ee143515881662c8f6dd7bf36ea823dc94e
Joas A Santos <34966120+CyberSecurityUP@users.noreply.github.com>
2024-07-27 00:38:14 -0300

    Update Bootloader Sample 4.asm

--- Bootloader Sample 4.asm
+++ Bootloader Sample 4.asm
@@ -2,12 +2,10 @@ bits 16
 org 0x7c00
 
 start:
-    ; Configuração inicial do segmento de dados
     xor ax, ax
     mov ds, ax
     mov es, ax
 
-    ; Limpa a tela
     mov ah, 0x06
     xor al, al
     xor cx, cx
@@ -15,7 +13,6 @@ start:
     mov bh, 0x1E
     int 0x10
 
-    ; Exibe o menu
     mov si, menu_options
 print_menu:
     lodsb
@@ -27,7 +24,7 @@ print_menu:
 
 wait_for_input:
     xor ah, ah
-    int 0x16    ; Espera uma entrada do teclado
+    int 0x16    
     cmp al, '1'
     je install_binary
     cmp al, '2'
@@ -35,21 +32,20 @@ wait_for_input:
     jmp print_menu
 
 install_binary:
-    ; Supõe que o binário está no setor 3 e copia para o setor 100
-    mov ah, 0x02   ; Função de leitura de disco
-    mov al, 1      ; Ler 1 setor
-    mov ch, 0      ; Cilindro 0
-    mov cl, 3      ; Setor 3
-    mov dh, 0      ; Cabeça 0
-    mov dl, 0x80   ; Drive (0x80 = primeiro disco rígido)
-    mov bx, 0x8000 ; Endereço na memória para carregar o binário
-    int 0x13       ; Interação do BIOS para leitura de disco
+    mov ah, 0x02   
+    mov al, 1      
+    mov ch, 0      
+    mov cl, 3      
+    mov dh, 0      
+    mov dl, 0x80   
+    mov bx, 0x8000 
+    int 0x13       
 
-    mov ah, 0x03   ; Função de escrita de disco
-    mov al, 1      ; Escrever 1 setor
-    mov ch, 0      ; Cilindro 0
-    mov cl, 100    ; Setor 100
-    int 0x13       ; Interação do BIOS para escrita de disco
+    mov ah, 0x03   
+    mov al, 1      
+    mov ch, 0      
+    mov cl, 100    
+    int 0x13      
     jmp success
 
 display_message:
